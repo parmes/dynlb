302a8b052e4f6cd77a12dd5078e4e601490a7a5f
Tomasz Koziara <t.koziara@gmail.com>
2016-05-06 14:32:45 +0200

    Licensing and copyright unhomogenized.

--- rcb.h
+++ rcb.h
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2016 Tomasz Koziara
+Copyright (c) 2016 EDF Energy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
